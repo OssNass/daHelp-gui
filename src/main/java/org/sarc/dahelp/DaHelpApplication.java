2f5d65b3727579e1a358fca9da6b77e2670461ff
Ossama Nasser <ossnass@gmail.com>
2020-05-20 20:31:25 +0300

    التهيئة الأساسية
    تهيئة أساسية للنظام

---
+++ src/main/java/org/sarc/dahelp/DaHelpApplication.java
@@ -0,0 +1,13 @@
+package org.sarc.dahelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaHelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sarc/dahelp/DaHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sarc.dahelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
